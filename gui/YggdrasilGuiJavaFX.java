5ff723b8761f0500ab0bd563b951839d1b2a7b23
formim <formim@rpi.edu>
2018-04-28 16:53:51 -0400

    Add files via upload
    Can now add lines.

--- gui/YggdrasilGuiJavaFX.java
+++ gui/YggdrasilGuiJavaFX.java
@@ -12,13 +12,13 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCombination;
@@ -33,6 +33,204 @@ import javafx.stage.Stage;
  *
  * @author Mario
  */
+
+class Pants extends VBox{
+    Pants(){
+        pants_node_width = 130;
+        pants_node_spacing = 5;
+        button_width = (pants_node_width / 3) - pants_node_spacing;
+        text_highlight = "white"; selected = false;
+        statements = new VBox();
+        statements.setSpacing(1);
+        visual = makePants();
+    }
+    
+    private MenuButton makeTextFieldMenus(){
+        MenuButton rcl_menu = new MenuButton();
+        rcl_menu.setMinWidth(1);
+        rcl_menu.setMaxWidth(23);
+        
+        MenuItem premise = new MenuItem("Premise");
+        premise.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // Specify that statement is a premise, action, etc
+            }
+        });
+        MenuItem contr = new MenuItem("Contradiction");
+        contr.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        
+        // Make Intro and Elim submenus
+        Menu decomp = new Menu("Decomp. Rules");
+        MenuItem and_dc = new MenuItem("∧");
+        and_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // Select statement and enter "and_dc select" state
+                select();
+            }
+        });
+        MenuItem or_dc = new MenuItem("-");
+        or_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        MenuItem cond_dc = new MenuItem("-");
+        cond_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        MenuItem neg_and_dc = new MenuItem("~&");
+        neg_and_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        MenuItem neg_or_dc = new MenuItem("~|");
+        neg_or_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        MenuItem neg_cond_dc = new MenuItem("~-");
+        neg_cond_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                // ''
+            }
+        });
+        MenuItem bic_dc = new MenuItem("--");
+        bic_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                //
+            }
+        });
+        MenuItem neg_bic_dc = new MenuItem("");
+        neg_bic_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                //
+            }
+        });
+        MenuItem dneg_dc = new MenuItem("");
+        dneg_dc.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent t){
+                //
+            }
+        });
+        
+        decomp.getItems().addAll(and_dc, or_dc, cond_dc, neg_and_dc, neg_or_dc, neg_cond_dc, bic_dc, neg_bic_dc, dneg_dc);
+        
+        rcl_menu.getItems().addAll(premise, contr, decomp);
+        return rcl_menu;
+    }
+    
+    private HBox makeTextView(){
+        HBox hb_row = new HBox();
+        hb_row.setSpacing(0);
+        TextField enter_st = new TextField("Enter Statement");
+        enter_st.setPrefWidth(pants_node_width - 23);
+        //enter_st.setStyle("-fx-text-fill: red");
+        
+        hb_row.getChildren().addAll(enter_st, makeTextFieldMenus());
+        return hb_row;
+    }
+    
+    private VBox makePants(){
+        VBox pants = new VBox();
+        //pants.setPrefSize(100, 80);
+        pants.setAlignment(Pos.CENTER);
+        
+        // Initialize text entry and textbox properties;
+        HBox init_row = makeTextView();
+        statements.getChildren().add(init_row);
+        
+        // Buttons and button properties;
+        /*Button close_br = new Button("X");
+        close_br.setPrefWidth(button_width); 
+        Button open_br = new Button("O");
+        open_br.setPrefWidth(button_width);*/
+        Button finish_br = new Button("✓");
+        finish_br.setPrefWidth(button_width);
+        // Stand-in for deselect.  Remove 
+        finish_br.setOnAction(new EventHandler<ActionEvent>(){
+            @Override public void handle(ActionEvent e){
+                deselect();
+            }
+        });
+        Button more_br = new Button("⋏");
+        more_br.setPrefWidth(button_width);
+        Button add_st = new Button("+");
+        add_st.setPrefWidth(button_width);
+        add_st.setOnAction(new EventHandler<ActionEvent>(){
+            @Override public void handle(ActionEvent e){
+                addStatement();
+            }
+        });
+            // Button Tooltips (Hover Over)
+        Tooltip fb_tt = new Tooltip("Finish Branch");
+        Tooltip mb_tt = new Tooltip("Make Branch");
+        Tooltip as_tt = new Tooltip("Add Statement");
+        Tooltip.install(more_br, mb_tt);
+        Tooltip.install(finish_br, fb_tt);
+        Tooltip.install(add_st, as_tt);
+        
+        HBox hb_buttons = new HBox();
+        hb_buttons.getChildren().addAll(add_st, more_br, finish_br);
+        hb_buttons.setSpacing(pants_node_spacing);
+        hb_buttons.setAlignment(Pos.CENTER);
+
+        pants.getChildren().addAll(statements, hb_buttons);
+        String vb_border = "-fx-border-color: grey;\n" +
+                           "-fx-border-insets: 5;\n" +
+                           "-fx-border-width: 1;\n" +
+                           "-fx-border-radius: 3;\n" +
+                           "-fx-border-style: solid;\n"; 
+ 
+        pants.setStyle(vb_border);
+        pants.setPadding(new Insets(pants_node_spacing, pants_node_spacing, 
+                                    pants_node_spacing, pants_node_spacing));
+        pants.setSpacing(pants_node_spacing);
+        
+        return pants;
+    }
+    
+    private void addStatement(){
+        HBox new_st = makeTextView();
+        statements.getChildren().add(new_st);
+    }
+    
+    private void select(){
+        text_highlight = "yellow";
+    }
+    
+    private void deselect(){
+        text_highlight = "white";
+    }
+    
+    double pants_node_width;
+    double pants_node_spacing;
+    double button_width;
+    String text_highlight;
+    boolean selected;
+    VBox visual;
+    VBox statements;
+    
+}
+
 public class YggdrasilGuiJavaFX extends Application {
     
     // To add click-drag functionality, declare variables
@@ -42,223 +240,45 @@ public class YggdrasilGuiJavaFX extends Application {
     private double nodeX = 0;
     private double nodeY = 0;
     
+    /*
     double pants_node_spacing = 5;
     double pants_node_width = 100;
-    
-    String text_highlight = "white";
+    String text_highlight = "white"; */
     
     BorderPane borderpane;
-    VBox pants;
+    Group drawer; // Holds pants.
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Yggdrasil");
         
         borderpane = new BorderPane();
-       
+        drawer = new Group();
+        drawer.setLayoutX(230);
+        drawer.setLayoutY(40);
         //Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         //root.getChildren().add();
         
         Group root = new Group();
-        
         SubScene subscene = new SubScene(root, 300, 300);
         
         // Make menu bar
         MenuBar menubar = makeMenuBar();
         menubar.prefWidthProperty().bind(stage.widthProperty());
-        // Make some pants
-        pants = makePants();
-        
+        // Make some pants and add it to drawer
+        Pants pants = new Pants();
+        drawer.getChildren().add(pants.visual);
         borderpane.setTop(menubar);
         
         // Add event handlesers
-        pants.setOnMousePressed(mouseClick());
-        pants.setOnMouseDragged(mouseDrag());
+        drawer.setOnMousePressed(mouseClick());
+        drawer.setOnMouseDragged(mouseDrag());
         
         Scene scene = new Scene(root, 600, 500);
-        root.getChildren().add(pants);
+        root.getChildren().add(drawer);
         root.getChildren().add(borderpane);
         
         stage.setScene(scene);
         stage.show();
-    }
-    
-    private VBox makePants(){
-        VBox pants = new VBox();
-        //pants.setPrefSize(100, 80);
-        pants.setAlignment(Pos.CENTER);
-        
-        // Initialize text entry and textbox properties;
-        TextField enter_st = new TextField("Enter Statement");
-        enter_st.setMaxWidth(pants_node_width);
-        enter_st.setStyle("-fx-control-inner-background: " + text_highlight);
-        
-        enter_st.setContextMenu(makeTextFieldMenus());
-        
-        // Buttons and button properties;
-        double button_width = (pants_node_width / 3) - pants_node_spacing;
-        Button close_br = new Button("X");
-        close_br.setPrefWidth(button_width);
-        Button open_br = new Button("O");
-        open_br.setPrefWidth(button_width);
-        Button more_br = new Button("+");
-        more_br.setPrefWidth(button_width);
-            // Button Tooltips (Hover Over)
-        Tooltip cb_tt = new Tooltip("Close Branch");
-        Tooltip ob_tt = new Tooltip("Open Branch");
-        Tooltip mb_tt = new Tooltip("New Branch");
-        Tooltip.install(close_br, cb_tt);
-        Tooltip.install(open_br, ob_tt);
-        Tooltip.install(more_br, mb_tt);
-
-        HBox hb_buttons = new HBox();
-        hb_buttons.getChildren().addAll(close_br, open_br, more_br);
-        hb_buttons.setSpacing(pants_node_spacing);
-        hb_buttons.setAlignment(Pos.CENTER);
-
-        pants.getChildren().addAll(enter_st, hb_buttons);
-        String vb_border = "-fx-border-color: grey;\n" +
-                           "-fx-border-insets: 5;\n" +
-                           "-fx-border-width: 1;\n" +
-                           "-fx-border-style: solid;\n"; 
- 
-        pants.setStyle(vb_border);
-        pants.setPadding(new Insets(pants_node_spacing, pants_node_spacing, 
-                                    pants_node_spacing, pants_node_spacing));
-        pants.setSpacing(pants_node_spacing);
-        // Initial X and Y.
-        pants.setLayoutX(120);
-        pants.setLayoutY(50);
-        
-        return pants;
-    }
-    
-    private ContextMenu makeTextFieldMenus(){
-        ContextMenu rcl_menu = new ContextMenu();
-        
-        MenuItem premise = new MenuItem("Premise");
-        premise.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify that statement is a premise, action, etc
-            }
-        });
-        
-        // Make Intro and Elim submenus
-        Menu intro = new Menu("Intro");
-        MenuItem and_i = new MenuItem("∧");
-        and_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify And Intro
-            }
-        });
-        MenuItem or_i = new MenuItem("∨");
-        or_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Or Intro
-            }
-        });
-        MenuItem neg_i = new MenuItem("¬");
-        neg_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Negation Intro
-            }
-        });
-        MenuItem cont_i = new MenuItem("⊥");
-        cont_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Contradiction Intro
-            }
-        });
-        MenuItem cond_i = new MenuItem("→");
-        cond_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Conditional Intro
-            }
-        });
-        MenuItem bic_i = new MenuItem("↔");
-        bic_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Biconditional Intro
-            }
-        });
-        MenuItem eq_i = new MenuItem("=");
-        eq_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify = Intro
-            }
-        });
-        MenuItem unq_i = new MenuItem("∀");
-        unq_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Universal Quantifier Intro
-            }
-        });
-        MenuItem exq_i = new MenuItem("∃");
-        exq_i.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Existential Quantifier Intro
-            }
-        });
-        intro.getItems().addAll(and_i, or_i, neg_i, cont_i, cond_i, bic_i, eq_i, unq_i, exq_i);
-        
-        Menu elim = new Menu("Elim");
-        MenuItem and_e = new MenuItem("∧");
-        and_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify And Elim
-            }
-        });
-        MenuItem or_e = new MenuItem("∨");
-        or_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Or Elim
-            }
-        });
-        MenuItem neg_e = new MenuItem("¬");
-        neg_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Negation Elim
-            }
-        });
-        MenuItem cont_e = new MenuItem("⊥");
-        cont_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Contradiction Elim
-            }
-        });
-        MenuItem cond_e = new MenuItem("→");
-        cond_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Conditional Elim
-            }
-        });
-        MenuItem bic_e = new MenuItem("↔");
-        bic_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Biconditional Elim
-            }
-        });
-        MenuItem eq_e = new MenuItem("=");
-        eq_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify = Elim
-            }
-        });
-        MenuItem unq_e = new MenuItem("∀");
-        unq_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Universal Quantifier Elim
-            }
-        });
-        MenuItem exq_e = new MenuItem("∃");
-        exq_e.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
-                // Specify Existential Quantifier Elim
-            }
-        });
-        elim.getItems().addAll(and_e, or_e, neg_e, cont_e, cond_e, bic_e, eq_e, unq_e, exq_e);
-        
-        rcl_menu.getItems().addAll(premise, intro, elim);
-        return rcl_menu;
     }
     
     private MenuBar makeMenuBar(){
@@ -350,8 +370,8 @@ public class YggdrasilGuiJavaFX extends Application {
                    mouseY = event.getSceneY();
                    
                    // Get node coordinates
-                   nodeX = pants.getLayoutX();
-                   nodeY = pants.getLayoutY();
+                   nodeX = drawer.getLayoutX();
+                   nodeY = drawer.getLayoutY();
                 }
             } 
         };
@@ -367,8 +387,8 @@ public class YggdrasilGuiJavaFX extends Application {
                     // Update pants coordinates.
                     nodeX += difX;
                     nodeY += difY;
-                    pants.setLayoutX(nodeX);
-                    pants.setLayoutY(nodeY);
+                    drawer.setLayoutX(nodeX);
+                    drawer.setLayoutY(nodeY);
                     // Update mouse coordinates.
                     mouseX = event.getSceneX();
                     mouseY = event.getSceneY();
